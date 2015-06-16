e4edd0038eece566e4bca3e2f1bce39e9b1342a9
fabio-baia <fabio.baia@benner.com.br>
2015-06-16 20:04:55 -0300

    Adicionada action Details que recebe parametro "id" configurado na rota

--- Treinamento/Controllers/WelcomeController.cs
+++ Treinamento/Controllers/WelcomeController.cs
@@ -15,8 +15,13 @@ namespace Treinamento.Controllers
 
         public string Browse(string genre)
         {
-            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            string message = HttpUtility.HtmlEncode("Welcome.Browse, Genre = " + genre);
             return message;
+        }
+
+        public string Details(int id)
+        {
+            return "Welcome.Details, ID = " + id;
         }
     }
 }
