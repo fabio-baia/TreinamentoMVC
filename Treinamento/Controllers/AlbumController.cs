70e7a8498610de782629424e6ac1318ae2f176b7
fabio-baia <fabio.baia@benner.com.br>
2015-06-16 22:51:24 -0300

    Adicionado Controller e Index do Album

---
+++ Treinamento/Controllers/AlbumController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Treinamento.Context;
+
+namespace Treinamento.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private LojaContext db = new LojaContext();
+
+        public ActionResult Index()
+        {
+            var albums = db.Albums.Include(a => a.Artista).Include(a => a.Genero);
+            return View(albums.ToList());
+        }
+
+    }
+}
