e92a472cfe61a71820dd6d6fe0dbda6a82143ca0
fabio-baia <fabio.baia@benner.com.br>
2015-06-18 19:32:09 -0300

    Adicionado exemplos de consulta usando linq e lambda

--- Treinamento/Controllers/AlbumController.cs
+++ Treinamento/Controllers/AlbumController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Treinamento.Context;
+using Treinamento.Models;
 
 namespace Treinamento.Controllers
 {
@@ -14,9 +15,25 @@ namespace Treinamento.Controllers
 
         public ActionResult Index()
         {
-            var albums = db.Albums.Include(a => a.Artista).Include(a => a.Genero);
+            //using linq
+            //var albums = from album in db.Albums
+            //    orderby album.Titulo ascending
+            //    select album;
+
+            //using lambda
+            var albums = db.Albums.OrderBy(q => q.Titulo);
+
+            //order with multi params and where clause
+            //var albums = db.Albums.OrderBy(q => new
+            //{
+            //    q.Titulo,
+            //    q.Genero.Descricao
+            //}).Where(q => q.Titulo.StartsWith("a"));
+
+            
             return View(albums.ToList());
         }
 
+        
     }
 }
