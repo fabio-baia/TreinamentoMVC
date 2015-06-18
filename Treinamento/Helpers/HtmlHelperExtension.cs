8bb26a9e0b9ffcf546c40875297b0b482936e124
fabio-baia <fabio.baia@benner.com.br>
2015-06-18 20:49:35 -0300

    Adicionada HtmlHelperExtension com método que cria "link voltar"

---
+++ Treinamento/Helpers/HtmlHelperExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Treinamento.Helpers
+{
+    public static class HtmlHelperExtension
+    {
+        public static MvcHtmlString LinkVoltar(this HtmlHelper html, string idLink, string textoLink = "Voltar")
+        {
+            var strLink = string.Format("<a id=\"{0}\" href=\"javascript:history.go(-1);\">{1}</a>",
+                idLink,
+                textoLink);
+
+            return new MvcHtmlString(strLink);
+        }
+    }
+}
