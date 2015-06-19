212c6f1e23e5efeeffb77edbe4aac5e48096e7dd
fabio-baia <fabio.baia@benner.com.br>
2015-06-18 22:36:09 -0300

    Adicionado GeneroController e view Index

---
+++ Treinamento/Controllers/GeneroController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Treinamento.Context;
+using Treinamento.Models;
+
+namespace Treinamento.Controllers
+{
+    public class GeneroController : Controller
+    {
+        private readonly LojaContext _db = new LojaContext();
+
+        public ActionResult Index()
+        {
+            return View(_db.Generos.ToList());
+        }
+
+    }
+}
