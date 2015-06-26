05ddb1d1c7e89b5f12e333c1a983ac82072d2d2d
fabio-baia <fabio.baia@benner.com.br>
2015-06-25 22:09:29 -0300

    Criado projeto Treinamento.Core Adicionado EntityFramework e Ninject Criado namespaces Model e Context Criado model Pessoa e classe de Context

---
+++ Treinamento.Core/Context/Context.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Treinamento.Core.Model;
+
+namespace Treinamento.Core.Context
+{
+    public class Context : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
