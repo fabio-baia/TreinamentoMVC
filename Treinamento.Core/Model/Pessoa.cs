8e6feccf9eba6ed0c93e306252b727198e31ea0e
fabio-baia <fabio.baia@benner.com.br>
2015-06-30 20:51:32 -0300

    Mapeando a Cidade no model Pessoa

--- Treinamento.Core/Model/Pessoa.cs
+++ Treinamento.Core/Model/Pessoa.cs
@@ -1,8 +1,15 @@
-﻿namespace Treinamento.Core.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Treinamento.Core.Model
 {
     public class Pessoa
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        public int IdCidade { get; set; }
+
+        [ForeignKey("IdCidade")]
+        public virtual Cidade Cidade { get; set; }
     }
 }
