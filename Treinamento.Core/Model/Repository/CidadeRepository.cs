8df0568dbbfcef86fb87f3be99563d8a4b3c4586
fabio-baia <fabio.baia@benner.com.br>
2015-06-30 20:54:21 -0300

    Adicionada interface ICidadeRepository e sua implementação CidadeRepository

---
+++ Treinamento.Core/Model/Repository/CidadeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Treinamento.Core.Model.Repository.Common;
+using Treinamento.Core.Model.Repository.Interface;
+
+namespace Treinamento.Core.Model.Repository
+{
+    public class CidadeRepository : Repository<Cidade>, ICidadeRepository
+    {
+
+    }
+}
---
+++ Treinamento.Core/Model/Repository/Interface/ICidadeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Treinamento.Core.Model.Repository.Common;
+
+namespace Treinamento.Core.Model.Repository.Interface
+{
+    public interface ICidadeRepository : IRepository<Cidade>
+    {
+
+    }
+}
