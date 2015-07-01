e55569d59651d48e45e067a40c8873e945983130
fabio-baia <fabio.baia@benner.com.br>
2015-06-30 21:59:50 -0300

    Adicionado scriptCidade que consulta os estados via ajax

---
+++ Treinamento/Scripts/Pessoa/scriptCidade.js
@@ -0,0 +1,11 @@
+﻿function ScriptCidade(idUf, idCidade, urlCidade) {
+    this.iniciar = function() {
+        $("#" + idUf).change(function() {
+            var uf = $(this).val();
+
+            $.post(urlCidade, {uf: uf}, function(options) {
+                $("#" + idCidade.html(options));
+            });
+        });
+    }
+}
