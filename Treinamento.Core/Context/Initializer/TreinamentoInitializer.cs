afbe31da8d76050a39da5f8e4e2813d913d51d83
fabio-baia <fabio.baia@benner.com.br>
2015-06-30 21:36:58 -0300

    Adicionando arquivos ao projeto

---
+++ Treinamento.Core/Context/Initializer/TreinamentoInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using Treinamento.Core.Model;
+
+namespace Treinamento.Core.Context.Initializer
+{
+    public class TreinamentoInitializer : DropCreateDatabaseAlways<TreinamentoContext>
+    {
+        protected override void Seed(TreinamentoContext context)
+        {
+            context.Cidades.Add(new Cidade{ Nome = "Maringá", Uf = "PR" });
+            context.Cidades.Add(new Cidade{ Nome = "Sarandi", Uf = "PR" });
+            context.Cidades.Add(new Cidade{ Nome = "São Paulo", Uf = "SP" });
+            context.Cidades.Add(new Cidade{ Nome = "Porto Alegre", Uf = "RS" });
+
+            base.Seed(context);
+        }
+    }
+}
--- Treinamento.Core/Context/TreinamentoContext.cs
+++ Treinamento.Core/Context/TreinamentoContext.cs
@@ -6,6 +6,7 @@ namespace Treinamento.Core.Context
     public class TreinamentoContext : DbContext
     {
         public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Cidade> Cidades { get; set; }
 
         public TreinamentoContext()
             : base("DefaultConnection")
