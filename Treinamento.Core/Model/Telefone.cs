796119e0135dc656d60f70b6346bb0a7e33f4a93
fabio-baia <fabio.baia@benner.com.br>
2015-06-30 22:54:23 -0300

    Adicionado model Telefone

---
+++ Treinamento.Core/Model/Telefone.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Treinamento.Core.Model
+{
+    public class Telefone
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+
+        public int IdPessoa { get; set; }
+        [ForeignKey("IdPessoa")]
+        public Pessoa Pessoa { get; set; }
+    }
+}
