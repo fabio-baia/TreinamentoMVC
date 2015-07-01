72d35d1e65c037620543f7ac28b0414db6961c58
fabio-baia <fabio.baia@benner.com.br>
2015-06-30 21:34:00 -0300

    Adicionado controller Cidade e partial view

---
+++ Treinamento/Controllers/CidadeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Treinamento.Core.Service.Interfaces;
+
+namespace Treinamento.Controllers
+{
+    public class CidadeController : Controller
+    {
+        private readonly ICidadeService _cidadeService;
+
+        public CidadeController(ICidadeService cidadeService)
+        {
+            _cidadeService = cidadeService;
+        }
+
+        public ActionResult AjaxOption(string uf)
+        {
+            return PartialView(_cidadeService.Query(q => q.Uf == uf));
+        }
+
+    }
+}
