b8c85537b1f1b21d81eabba0aeee9db851e1f66c
fabio-baia <fabio.baia@benner.com.br>
2015-07-02 19:23:24 -0300

    Adicionado ITelefoneService, ITelefoneRepository e suas implementações

---
+++ Treinamento.Core/Model/Repository/Interface/ITelefoneRepository.cs
@@ -0,0 +1,9 @@
+﻿using Treinamento.Core.Model.Repository.Common;
+
+namespace Treinamento.Core.Model.Repository.Interface
+{
+    public interface ITelefoneRepository : IRepository<Telefone>
+    {
+
+    }
+}
---
+++ Treinamento.Core/Service/TelefoneService.cs
@@ -0,0 +1,15 @@
+﻿using Treinamento.Core.Model;
+using Treinamento.Core.Model.Repository.Interface;
+using Treinamento.Core.Service.Common;
+using Treinamento.Core.Service.Interfaces;
+
+namespace Treinamento.Core.Service
+{
+    public class TelefoneService : Service<Telefone>, ITelefoneService
+    {
+        public TelefoneService(ITelefoneRepository repository)
+            : base(repository)
+        {
+        }
+    }
+}
