028b4805952b4ec2185d9634946fb250eccef352
fabio-baia <fabio.baia@benner.com.br>
2015-07-02 19:22:33 -0300

    Refatorando PessoaService e Cidade Service para utilizar service genérico

--- Treinamento.Core/Service/PessoaService.cs
+++ Treinamento.Core/Service/PessoaService.cs
@@ -1,69 +1,15 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using Treinamento.Core.Model;
+﻿using Treinamento.Core.Model;
 using Treinamento.Core.Model.Repository.Interface;
+using Treinamento.Core.Service.Common;
 using Treinamento.Core.Service.Interfaces;
 
 namespace Treinamento.Core.Service
 {
-    public class PessoaService : IPessoaService
+    public class PessoaService : Service<Pessoa>, IPessoaService
     {
-        private readonly IPessoaRepository _repository;
-
         public PessoaService(IPessoaRepository repository)
+            : base(repository)
         {
-            _repository = repository;
-        }
-
-        public Pessoa Find(int id)
-        {
-            return _repository.Find(id);
-        }
-
-        public IQueryable<Pessoa> All(bool @readonly = false)
-        {
-            return _repository.All(@readonly);
-        }
-
-        public IQueryable<Pessoa> Query(Expression<Func<Pessoa, bool>> predicate, bool @readonly = false)
-        {
-            return _repository.Query(predicate, @readonly);
-        }
-
-        public void Save(Pessoa entity)
-        {
-            _repository.BeginTransaction();
-            _repository.Save(entity);
-            _repository.Commit();
-        }
-
-        public void Create(Pessoa entity)
-        {
-            _repository.BeginTransaction();
-            _repository.Add(entity);
-            _repository.Commit();
-        }
-
-        public void Update(Pessoa entity)
-        {
-            _repository.BeginTransaction();
-            _repository.Update(entity);
-            _repository.Commit();
-        }
-
-        public void Delete(int id)
-        {
-            _repository.BeginTransaction();
-            _repository.Delete(id);
-            _repository.Commit();
-        }
-
-        public void Delete(Pessoa entity)
-        {
-            _repository.BeginTransaction();
-            _repository.Delete(entity);
-            _repository.Commit();
         }
     }
 }
