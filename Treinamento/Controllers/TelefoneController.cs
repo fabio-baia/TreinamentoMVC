38bad82709750a1caebb1292cd1cddb3788f27a0
fabio-baia <fabio.baia@benner.com.br>
2015-07-02 21:02:19 -0300

    Adicionando TelefoneController

---
+++ Treinamento/Controllers/TelefoneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Treinamento.Core.Model;
+using Treinamento.Core.Service.Interfaces;
+
+namespace Treinamento.Controllers
+{
+    public class TelefoneController : Controller
+    {
+        private readonly ITelefoneService telefoneService;
+
+        public TelefoneController(ITelefoneService telefoneService)
+        {
+            this.telefoneService = telefoneService;
+        }
+
+        public ActionResult AjaxAdd(Telefone telefone)
+        {
+            if (!ModelState.IsValid)
+                return Json(new{Message = ModelState.Values.Select(q => q.Value), Status = false});
+
+            telefoneService.Save(telefone);
+            return Json(new{Status = true});
+        }
+
+        public ActionResult AjaxRemove(int id)
+        {
+            telefoneService.Delete(id);
+            return Json(new{Status = true}, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AjaxList(int id)
+        {
+            return PartialView(telefoneService.Query(q => q.IdPessoa == id));
+        }
+
+        public ActionResult DialogForm()
+        {
+            return PartialView(new Telefone());
+        }
+    }
+}
