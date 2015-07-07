0587ffbc99dc8a0052b0d356c75c49a3c7747936
fabio-baia <fabio.baia@benner.com.br>
2015-07-07 19:59:54 -0300

    Iniciando com angular

---
+++ Treinamento/Controllers/AngularJsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Treinamento.Controllers
+{
+    public class AngularJsController : Controller
+    {
+        public ActionResult Intro()
+        {
+            return View();
+        }
+
+        public ActionResult Expression()
+        {
+            return View();
+        }
+
+        public ActionResult Application()
+        {
+            return View();
+        }
+
+        public ActionResult Repetindo()
+        {
+            return View();
+        }
+    }
+}
