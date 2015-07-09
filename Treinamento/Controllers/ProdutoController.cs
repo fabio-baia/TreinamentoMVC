9b2de73c48d2c6c964ed2a1c93c3a3358f002c5b
fabio-baia <fabio.baia@benner.com.br>
2015-07-09 19:55:26 -0300

    Implementando ProdutoController e views de template para o angular

--- Treinamento/Controllers/ProdutoController.cs
+++ Treinamento/Controllers/ProdutoController.cs
@@ -4,6 +4,24 @@ namespace Treinamento.Controllers
 {
     public class ProdutoController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
 
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
     }
 }
