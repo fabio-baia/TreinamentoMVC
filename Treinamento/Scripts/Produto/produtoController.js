87237d9b4eb24fd17a50c8a3fb74162c6e921385
fabio-baia <fabio.baia@benner.com.br>
2015-07-09 20:14:05 -0300

    Adicionado produtoController.js com configurações das rotas do angular

---
+++ Treinamento/Scripts/Produto/produtoController.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/list', {
+        templateUrl: '/Produto/List',
+        controller: 'ListController'
+    }).when('/create', {
+        templateUrl: '/Produto/Edit',
+        controller: 'EditController'
+    }).when('/edit/:id', {
+        templateUrl: '/Produto/Edit',
+        controller: 'EditController'
+    }).when('/delete/:id', {
+        templateUrl: '/Produto/Delete',
+        controller: 'DeleteController'
+    }).otherwise({
+        redirectTo: '/list'
+    });
+});
+
