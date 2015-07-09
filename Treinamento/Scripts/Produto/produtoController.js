833dc428b3bfdcfc5dbd9f36409cbc85ff65e099
fabio-baia <fabio.baia@benner.com.br>
2015-07-09 20:23:46 -0300

    Adicionando angular controllers para operações de editar, adicionar e listar

--- Treinamento/Scripts/Produto/produtoController.js
+++ Treinamento/Scripts/Produto/produtoController.js
@@ -18,3 +18,48 @@ app.config(function ($routeProvider) {
     });
 });
 
+app.controller('ListController', function ($scope, $http) {
+    $http.get('/Api/Produto').success(function (data) {
+        $scope.Produtos = data;
+    });
+});
+
+app.controller('EditController', function ($scope, $http, $routeParams, $location) {
+    var id = $routeParams.id;
+
+    if (id > 0) {
+        $scope.title = "Editando produto";
+
+        $http.get('/Api/Produto/' + id).success(function(data) {
+            $scope.Produto = data;
+        });
+    } else {
+        $scope.title = "Novo produto";
+    }
+
+    function create() {
+        $http.post('/Api/Produto', $scope.Produto).success(function () {
+            $location.path('/list');
+        }).error(function (data) {
+            $scope.error = data.ExceptionMessage;
+        });
+    }
+
+    function update() {
+        $http.put('/Api/Produto', $scope.Produto).success(function () {
+            $location.path('/list');
+        }).error(function (data) {
+            $scope.error = data.ExceptionMessage;
+        });
+    }
+
+    $scope.save = function() {
+        if ($scope.Produto.Id === 0)
+            create();
+        else
+            update();
+    }
+
+    
+
+});
