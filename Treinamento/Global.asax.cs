68c41e8396062285168f9201f7a1acc0f071c124
fabio-baia <fabio.baia@benner.com.br>
2015-07-09 21:21:25 -0300

    Adicionada configuração no Global.asax pra utlizar formato json como padrão

--- Treinamento/Global.asax.cs
+++ Treinamento/Global.asax.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
+﻿using System.Data.Entity;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Treinamento.App_Start;
-using Treinamento.Context;
 using Treinamento.Context.Initializer;
 using Treinamento.Core.Context.Initializer;
 
@@ -17,7 +13,7 @@ namespace Treinamento
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -30,6 +26,8 @@ namespace Treinamento
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             BinderConfig.Register();
+
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
         }
     }
 }
