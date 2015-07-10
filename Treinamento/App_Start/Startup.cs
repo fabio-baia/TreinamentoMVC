e2619b09ad1162ec5d53196aa150e5a68a8cf348
fabio-baia <fabio.baia@benner.com.br>
2015-07-09 22:28:36 -0300

    Adicionado arquivo Startup para inicialização do SignalR

---
+++ Treinamento/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Treinamento;
+
+[assembly:OwinStartup(typeof(Startup))]
+namespace Treinamento
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
