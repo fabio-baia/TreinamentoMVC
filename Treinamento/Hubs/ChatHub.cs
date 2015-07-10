73c1fc8cbd7a103eff4f980247e5a185d181718d
fabio-baia <fabio.baia@benner.com.br>
2015-07-09 22:09:42 -0300

    Adicionados ChatController, ChatHub e view 'Index' para o chat

---
+++ Treinamento/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+
+namespace Treinamento.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            var date = DateTime.Now.ToString("T");
+            Clients.All.broadcastMessage(name, message, date);
+        }
+    }
+}
